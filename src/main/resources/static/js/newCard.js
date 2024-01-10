f6d3a1c4da7c6927c8a42591b1166caa575a8cd6
Maxim Fisenko <fisish@yandex.ru>
2024-01-10 15:44:16 +0300

    feat: opening a new card works

--- src/main/resources/static/js/newCard.js
+++ src/main/resources/static/js/newCard.js
@@ -2,28 +2,45 @@ document.addEventListener("DOMContentLoaded", function (){
 
     fetchUserAccounts();
 
-    const accountNumber = document.getElementById("card-account");
-    const cardName = document.getElementById("card-name");
+    document.getElementById("new-card-form").addEventListener("submit", function (event) {
 
-    const data = {
-        accountNumber: accountNumber.value,
-        cardName: cardName.value
-    }
+        const accountSelect = document.getElementById("card-account");
+        const accountNumber = accountSelect.options[accountSelect.selectedIndex].value;
+        const cardName = document.getElementById("card-name").value;
 
-    fetch("/debitCards/open", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    })
-        .then(response => response.json())
-        .then(data => {console.log(data)})
-        .catch(error => {console.log(error)});
+        if (cardName !== null && cardName.trim() !== '') {
+
+            const data = {
+                accountNumber: accountNumber,
+                cardName: cardName
+            };
+
+            fetch("/debitCards/open", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                })
+                .catch(error => {
+                    console.log(error)
+                });
+
+            alert("The new card has been successfully created!");
+
+        } else {
+            alert("The name of the card cannot be empty!");
+        }
+
+    });
 
     function fetchUserAccounts() {
 
-        const userId = 2;
+        const userId = 1;
         const apiUrl = `accounts/user/${userId}`;
 
         const cardAccountSelect = document.getElementById("card-account");
@@ -33,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function (){
             .then(data => {
                 data.forEach(account => {
                     const option = document.createElement("option");
-                    option.value = account.id;
+                    option.value = account.number;
                     option.text = account.name;
                     cardAccountSelect.appendChild(option);
                 });
